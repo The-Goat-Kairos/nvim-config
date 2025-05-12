bbfce590f8161d69efad8ff31f798df53e381efc
Kairos <danielroepke01@gmail.com>
2025-05-12 10:43:06 +0200

    Updated keybinds

--- lua/kairos/remap.lua
+++ lua/kairos/remap.lua
@@ -4,18 +4,16 @@ vim.keymap.set("n", "<BS><BS>", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("n", "~", "~h")
+--vim.keymap.set("n", "~", "~h")
 
 vim.keymap.set("v", ">>", ">><Esc>V")
 vim.keymap.set("v", "<<", "<<<Esc>V")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-g>", "<C-u>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
@@ -28,3 +26,5 @@ vim.keymap.set("i", "<C-c>", "<Esc>") -- People not gonna like this one
 vim.keymap.set("n", "<leader>f", [[:!mpv ~/.config/nvim/lua/kairos/assets/'fuck.mp3'<CR>]])
 
 vim.keymap.set("n", "<C-O>", "O<Esc>0")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
