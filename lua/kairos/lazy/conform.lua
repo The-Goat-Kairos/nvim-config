8e6c017e177bf55d81e08b1af7f12649fd01e2f5
Kairos <danielroepke01@gmail.com>
2025-09-06 16:01:17 +0200

    automated dev commit

--- lua/kairos/lazy/conform.lua
+++ lua/kairos/lazy/conform.lua
@@ -4,6 +4,8 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+                c = { "clang-format" },
+                haskell = { "ormolu" },
 				lua = { "my_stylua", stop_after_first = true },
 				javascript = { "my_prettierd", stop_after_first = true },
 			},
