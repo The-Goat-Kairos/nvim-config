21661dfc9b23faf225e6adec03d3b52118e61a86
Kairos <danielroepke01@gmail.com>
2025-10-22 12:13:13 +0200

    automated dev commit

--- lua/kairos/lazy/conform.lua
+++ lua/kairos/lazy/conform.lua
@@ -1,34 +1,34 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
                 c = { "clang-format" },
                 haskell = { "my_ormolu" },
                 html = { "html_beautify" },
-				lua = { "my_stylua", stop_after_first = true },
-				javascript = { "my_prettierd", stop_after_first = true },
-			},
-			formatters = {
+                lua = { "my_stylua", stop_after_first = true },
+                javascript = { "my_prettierd", stop_after_first = true },
+            },
+            formatters = {
                 my_ormolu = {
                     command = "ormolu",
                     stdin = true,
-                    args = { }
+                    args = {},
                 },
                 my_stylua = {
                     command = "stylua",
-                    args = { "--config-path", "/home/kairos/.config/nvim/lua/kairos/utils/stylua.toml", "$FILENAME" }
+                    args = { "--config-path", "/home/kairos/.config/nvim/lua/kairos/utils/stylua.toml", "$FILENAME" },
                 },
-				my_prettierd = {
-					command = "prettierd",
-					args = { "$FILENAME" },
-					stdin = true,
-					env = {
-                        PRETTIERD_DEFAULT_CONFIG="/home/kairos/.config/nvim/lua/kairos/utils/.prettierrc"
-					},
-				},
-			},
-		})
-	end,
+                my_prettierd = {
+                    command = "prettierd",
+                    args = { "$FILENAME" },
+                    stdin = true,
+                    env = {
+                        PRETTIERD_DEFAULT_CONFIG = "/home/kairos/.config/nvim/lua/kairos/utils/.prettierrc",
+                    },
+                },
+            },
+        })
+    end,
 }
