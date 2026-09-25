725ddccba3b7f3408c5de29d000dc91589dd8995
Kairos <danielroepke01@gmail.com>
2026-09-25 10:40:31 +0200

    automated dev commit

--- lua/kairos/lazy/harpoon.lua
+++ lua/kairos/lazy/harpoon.lua
@@ -7,11 +7,10 @@ return {
 
     config = function()
         local harpoon = require("harpoon")
-
         harpoon:setup()
 
         vim.keymap.set('n', '<leader>a', function () harpoon:list():add() end)
-        vim.keymap.set('n', '<C-e>', function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set('n', '<C-e>', function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open Harpoon Window" })
 
         vim.keymap.set('n', '<C-v>', function () harpoon:list():select(1) end)
         vim.keymap.set('n', '<C-b>', function () harpoon:list():select(2) end)
